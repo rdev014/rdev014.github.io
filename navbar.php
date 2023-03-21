1452e01b73823d427d928dbff9b4066085185561
rahdev2021 <111628480+rahdev2021@users.noreply.github.com>
2023-03-21 16:03:39 +0530

    fy

---
+++ navbar.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <script src="bootstrap.bundle.min.js"></script>
+  <script src="https://kit.fontawesome.com/b124e05021.js" crossorigin="anonymous"></script>
+  <script async src="https://unpkg.com/es-module-shims@1.3.6/dist/es-module-shims.js"></script>
+  
+  <style>
+    body {
+      background: linear-gradient(to right, black 20%, navy);
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+
+    .head {
+      color: greenyellow;
+      border: 1px solid yellow;
+      box-shadow: 2px, 2px;
+      border-radius: 4px;
+      display: block;
+      float: right;
+      width: auto;
+      margin-left: 30em;
+      margin-top: -17em;
+      margin-right: 2em;
+      text-align: center;
+      padding: 18px;
+      font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+      letter-spacing: 2px;
+      font-size: 18px;
+    }
+
+
+    .d-flex {
+      width: auto;
+      height: auto;
+      
+
+    }
+
+    @media screen and (max-width:840px) {
+      .head {
+        width:fit-content;
+
+        margin-top: 3rem;
+        margin-left: 120px;
+
+      }
+
+      .d-flex{
+        display:fit-content
+        ;
+
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg " style="background:linear-gradient(to right,rgb(131, 66, 131),blue)">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="anime.png" alt="" height="200px" width="350px" class="img-fluid img">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"
+        style="color: aliceblue; background-color: aliceblue;">
+        <span class="navbar-toggler-icon" style="color: aliceblue;">
+
+        </span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav" style="gap: 40px;">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#" style="color: white; font-size: 26px;">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#foot" style="color: white; font-size: 26px;">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" style="color: white; font-size: 26px;">CONTACT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="blogs.html" style="color: white; font-size: 26px;">BLOGS</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav><br>
+<php
+    <div class="container-fluid " style="width: auto;">
+    <h1 class="hd1" style="color: yellowgreen; text-align: center; width: auto; height: auto;"> Welcome To Mistery</h1>
+    <br><br><br><br>
+    <div class="right" style="box-shadow: 20px white;" style="margin-right: 40rem;">
+      <img src="moon.gif" alt="" class="img-fluid col-lg-5 pic">
+    </div>
+    <div class="head left">
+      <h2>let's Explore</h2><br>
+      <p>Welcome to the world of mystery <br>
+        let's flow n fell the Superious power of huge planets like <b>Sun,Moon,Earth,Mars</b> Fell the energy of <b>Galaxies,stars and other various planets.. </b>
+      </p><br>
+      <div class="icon" ">
+              <a href=" blogs.html"><button style=" background-color: rgb(224, 224, 26); width:14rem; border: 2px solid black; border-radius:
+        20px;"><i class="fa-sharp fa-3x fa-solid fa-arrow-right-long"></i></button></a>
+      </div>
+    </div>
+
+  </div>
+  <hr class="mb-4">
+  <div class="row align-items-center">
+    <div class="col-md-4 col-lg-4">
+      <p>
+
+      </p>
+    </div>
+  </div>
+  <div class="container-xxl mt-4 container-fluid">
+    <h1 class="hd1"
+      style="color: orange; text-align: center; text-decoration: 1px solid white underline; font-family:Georgia, 'Times New Roman', Times, serif;">
+      The UnTold Space
+    </h1><br><br>
+    <!----->
+    <div class="container-fluid" style="margin: 0%; margin-right:0px; border-radius: 14px; border: 2px solid orange;">
+      <img src="solar.gif" alt="" class="img-fluid" width="1600">
+    </div>
+  </div>
+<br> <br>
+<br><br>
+<img src="https://i.gifer.com/P3F.gif" alt="" class="img-fluid" width="auto" height="auto">
+<img src="https://gfycat.com/hardtofindathleticamurratsnake" alt="">
+  <!--content-->
+
+  <div class="container-fluid row text-center text-md-left">
+  <div class="d-flex" style=" margin-top: 12em; width: auto; height: auto;">
+
+    <div class="sun">
+      <h1 style="color: orange;">Sun..</h1>
+      <img src="sunu.gif" alt="" class="img-fluid " width="160rem">
+    </div>
+    <div class="sun">
+      <h1 style="color:lightblue;"> Mars..</h1>
+      <img src="mars.gif" alt="" class="img-fluid " width="160rem">
+    </div>
+    <div class="sun">
+      <h1 style="color: red;"> Earth..</h1>
+      <img src="prithvi.gif" alt="" class="img-fluid" width="180rem">
+    </div>
+    <div class="mars" style="color:aliceblue;">
+      <h1> Moon..</h1>
+      <img src="moon.gif" alt="" class="img-fluid " width="180rem">
+    </div>
+    <div class="sun">
+      <h1 style="color:lawngreen">jupiter..</h1>
+      <img src="jupiter.gif" alt="" class="img-fluid " width="160rem">
+    </div>
+  </div>
+  </div>
+  <hr class="mb-4">
+  <div class="row align-items-center">
+    <div class="col-md-7 col-lg-8">
+      <p>
+
+      </p>
+    </div>
+  </div>
+  <!--Footer-->
+  <footer class="text-white pt-4 pb-4 mt-4"
+    style=" margin-top: 20em; background:linear-gradient(to right,blue,black 40%); margin-top: 10em; box-sizing: border-box; box-shadow: 2px 2px ;">
+
+    <div class="container text-center text-md-left">
+
+      <div class="row text-center text-md-left">
+
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning"> WElcome To Mystery</h5>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro vitae
+            quam perspiciatis dolor, fugit nobis dignissimos sapiente
+            incidunt eaque aperiam dolore. Harum, ipsa?
+          </p>
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 " id="foot">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Products</h5>
+          <p>
+            <a href="#" class="text-white" style="text-decoration: none;">Blogs</a><br>
+
+            <a href="#" class="text-white" style="text-decoration: none;">Refrences</a><br>
+
+            <a href="#" class="text-white" style="text-decoration: none;">Videos</a><br>
+
+            <a href="#" class="text-white" style="text-decoration: none;">Team Members</a>
+          </p>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning"> Usefull Links</h5>
+          <p>
+            <a href="#" class="text-white" style="text-decoration: none;">The Providers</a><br>
+
+            <a href="#" class="text-white" style="text-decoration: none;">The Providers</a><br>
+
+            <a href="#" class="text-white" style="text-decoration: none;">The Providers</a><br>
+
+            <a href="#" class="text-white" style="text-decoration: none;">The Providers</a><br>
+          </p>
+        </div>
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning"> Contact</h5>
+          <p>
+            <i class="fas fa-home mr-4 "></i>New York, NY 2333, US
+          </p>
+          <p>
+            <i class="fas fa-envelope mr-4 "></i>abc1344@gmail.com
+          </p>
+          <p>
+            <i class="fas fa-phone mr-4 "></i>+91 987312334
+          </p>
+          <p>
+            <i class="fas fa-print mr-4 "></i>+91246231125
+          </p>
+        </div>
+          </div>
+        </div>
+      </div>
+  </footer>
+  ?>
+
+</body>
+
+</html>
